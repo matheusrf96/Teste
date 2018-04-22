6cfdf58a67bf193296c433b52bfea53b899df56f
Matheus R <matheusrf96@gmail.com>
2018-04-22 19:31:17 -0300

    Atualização de relações

--- control/listar-contatos.php
+++ control/listar-contatos.php
@@ -17,6 +17,18 @@ $db->query("
 
 $contatos = $db->resultSet();
 
+$db->query("
+    SELECT usuario.username FROM usuario_grupo
+    INNER JOIN usuario ON usuario_grupo.usuario_id = usuario.id
+    INNER JOIN grupo ON usuario_grupo.grupo_id = grupo.id
+    WHERE usuario_grupo.membroAceito = 'A'
+    AND usuario_grupo.usuario_solicitante = ".$_SESSION['usuario']['id']."
+    AND usuario.id != ".$_SESSION['usuario']['id']."
+    AND usuario.usuarioAtivo = 1
+");
+
+$contatos += $db->resultSet();
+
 ?>
 
 <h3>Contatos</h3>
