ba139f925001d514f935629d986e126ac8bd8270
Matheus R <matheusrf96@gmail.com>
2018-04-30 00:28:40 -0300

    Correção solicitação de contato

--- control/listar-contatos.php
+++ control/listar-contatos.php
@@ -11,23 +11,26 @@ $db->query("
     INNER JOIN grupo ON usuario_grupo.grupo_id = grupo.id
     WHERE usuario_grupo.membroAceito = 'A'
     AND usuario_grupo.grupo_id != ".$_SESSION['usuario']['grupoPadrao']."
-    AND usuario_grupo.usuario_id = ".$_SESSION['usuario']['id']."
+    AND (
+        usuario_grupo.usuario_id = ".$_SESSION['usuario']['id']."
+        OR usuario_grupo.usuario_solicitante = ".$_SESSION['usuario']['id']."
+    )
     AND usuario.usuarioAtivo = 1
 ");
 
 $contatos = $db->resultSet();
 
-$db->query("
-    SELECT usuario.username FROM usuario_grupo
-    INNER JOIN usuario ON usuario_grupo.usuario_id = usuario.id
-    INNER JOIN grupo ON usuario_grupo.grupo_id = grupo.id
-    WHERE usuario_grupo.membroAceito = 'A'
-    AND usuario_grupo.usuario_solicitante = ".$_SESSION['usuario']['id']."
-    AND usuario.id != ".$_SESSION['usuario']['id']."
-    AND usuario.usuarioAtivo = 1
-");
+// $db->query("
+//     SELECT usuario.username FROM usuario_grupo
+//     INNER JOIN usuario ON usuario_grupo.usuario_id = usuario.id
+//     INNER JOIN grupo ON usuario_grupo.grupo_id = grupo.id
+//     WHERE usuario_grupo.membroAceito = 'A'
+//     AND usuario_grupo.usuario_solicitante = ".$_SESSION['usuario']['id']."
+//     AND usuario.id != ".$_SESSION['usuario']['id']."
+//     AND usuario.usuarioAtivo = 1
+// ");
 
-$contatos += $db->resultSet();
+// $contatos += $db->resultSet();
 
 ?>
 
